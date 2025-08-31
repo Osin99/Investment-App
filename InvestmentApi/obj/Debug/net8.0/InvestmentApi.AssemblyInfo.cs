fd5700024b38b9ce5cd495e553bf9533c0ea687b
Osin99 <osinnn1999@gmail.com>
2025-08-31 15:54:51 +0200

    dodanie dockerfile

--- InvestmentApi/obj/Debug/net8.0/InvestmentApi.AssemblyInfo.cs
+++ InvestmentApi/obj/Debug/net8.0/InvestmentApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InvestmentApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d060a608a798b2c5083c60fe3d4ea3a54249060")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a21a71080609d74ed3238d9aa361f84fbc1eaec7")]
 [assembly: System.Reflection.AssemblyProductAttribute("InvestmentApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InvestmentApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
